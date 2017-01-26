2ebe01d128bf29d1b9692836f40690c51465fcfb
Bruno Brasil <brunobrasilweb@gmail.com>
2017-01-26 00:13:41 -0200

    Estrutura da Mascara e validação cpf, cnpj e data

--- src/ng-formzin.angular.js
+++ src/ng-formzin.angular.js
@@ -15,6 +15,13 @@
     FormzinService.$inject = [];    
     function FormzinService() {
         return {
+            validate: function (val) {  
+                if (val == "") {
+                    return false;
+                }
+                
+                return true;
+            },
             validateEmail: function (email) {
                 var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
                 if (!pattern.test(email))
@@ -48,17 +55,17 @@
                         cpf === "99999999999")
                     return false;
 
-                add = 0;
-                for (i = 0; i < 9; i ++)
+                var add = 0;
+                for (var i = 0; i < 9; i ++)
                     add += parseInt(cpf.charAt(i)) * (10 - i);
-                rev = 11 - (add % 11);
+                var rev = 11 - (add % 11);
                 if (rev === 10 || rev === 11)
                     rev = 0;
                 if (rev !== parseInt(cpf.charAt(9)))
                     return false;
 
                 add = 0;
-                for (i = 0; i < 10; i ++)
+                for (var i = 0; i < 10; i ++)
                     add += parseInt(cpf.charAt(i)) * (11 - i);
                 rev = 11 - (add % 11);
                 if (rev === 10 || rev === 11)
@@ -69,6 +76,7 @@
                 return true;
             },
             validateCnpj: function (cnpj) {
+                console.log("validando");
                 cnpj = cnpj.replace(/[^\d]+/g, '');
 
                 if (cnpj == '')
@@ -91,17 +99,17 @@
                     return false;
 
                 // Valida DVs
-                tamanho = cnpj.length - 2
-                numeros = cnpj.substring(0, tamanho);
-                digitos = cnpj.substring(tamanho);
-                soma = 0;
-                pos = tamanho - 7;
-                for (i = tamanho; i >= 1; i--) {
+                var tamanho = cnpj.length - 2
+                var numeros = cnpj.substring(0, tamanho);
+                var digitos = cnpj.substring(tamanho);
+                var soma = 0;
+                var pos = tamanho - 7;
+                for (var i = tamanho; i >= 1; i--) {
                     soma += numeros.charAt(tamanho - i) * pos--;
                     if (pos < 2)
                         pos = 9;
                 }
-                resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                var resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
                 if (resultado != digitos.charAt(0))
                     return false;
 
@@ -109,7 +117,7 @@
                 numeros = cnpj.substring(0, tamanho);
                 soma = 0;
                 pos = tamanho - 7;
-                for (i = tamanho; i >= 1; i--) {
+                for (var i = tamanho; i >= 1; i--) {
                     soma += numeros.charAt(tamanho - i) * pos--;
                     if (pos < 2)
                         pos = 9;
@@ -225,9 +233,23 @@
                 }
             },
             parsers: function(value, mask) {
-                if (mask == 'cpf') {
+                if (['cpf','cnpj','postcode','phone','creditCard','custom'].indexOf(mask) != -1) {
                     return value.replace(/[^0-9]/g, "");
+                } else if (['date','datePtbr'].indexOf(mask) != -1) {
+                    var date = value.replace(/[^0-9]/g, "");
+                    
+                    if (date.length == 8) {
+                        return date.substring(4, 8) + "-" + date.substring(2, 4) + "-" + date.substring(0, 2);
+                    }
+                } else if (['dateEn'].indexOf(mask) != -1) {
+                    var date = value.replace(/[^0-9]/g, "");
+                    // 20000110
+                    if (date.length == 8) {
+                        return date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6, 8);
+                    }
                 }
+
+                return value;
             }   
         };    
     }
@@ -236,9 +258,9 @@
     function FormzinDirective($filter, FormzinService) {
         return {
             require: "ngModel",
-            scope: {
-                mask: '@'
-            },
+            restrict: 'A',
+            scope: {},
+            replace: false,
             link: function (scope, element, attrs, ctrl) {
                 var masksFormat = { 
                     "cpf": "000.000.000-00",
@@ -250,45 +272,96 @@
                     "custom": "custom",
                     "uppercase": "uppercase",
                     "date": "date",
+                    "datePtbr": "datePtbr",
+                    "dateEn": "dateEn",
                     "carPlate": "SSS 0000",
-                    "trackingCode": "SS000000000SS",
-                    "ccHolder": "uppercase",
-                    "ccNumber": "0000 0000 0000 0000",
-                    "ccPay": "00/00",
-                    "ccCcv": "int"
+                    "trackingCode": "SS000000000SS"
                 };
-                var mask = scope.mask;
+
+                var fieldsValidate = { 
+                    "cpf": FormzinService.validateCpf,
+                    "cnpj": FormzinService.validateCnpj,
+                    "postcode": FormzinService.validate,
+                    "phone": FormzinService.validate,
+                    "creditCard": FormzinService.validadeCreditCard,
+                    "int": FormzinService.validate,
+                    "date": FormzinService.validateDate,
+                };
+
+                var _attr = function(attr) {
+                    if (element.attr(attr)) {
+                        return element.attr(attr);
+                    } else if (element.attr("data-" + attr)) {
+                        return element.attr("data-" + attr);
+                    } else if (element.attr("ng-" + attr)) {
+                        return element.attr("ng-" + attr);
+                    }
+
+                    return null;
+                }
+
+                var _val = function() {
+                    var mask = _attr('mask');
+                    var val = element.val();
+                    if (undefined !== masksFormat[mask]) {
+                        var format = masksFormat[mask];
+                        
+                        if (format == 'datePtbr' || format == 'date') {
+                            var separator = '/';
+                            if (_attr('separator')) {
+                                separator = _attr('separator');
+                            }
+                            format = "00" + separator + "00" + separator + "0000";
+                        } else if (format == 'dateEn') {
+                            var separator = '-';
+                            if (_attr('separator')) {
+                                separator = _attr('separator');
+                            }
+                            format = "0000" + separator + "00" + separator + "00";
+                        }   
+
+                        val = FormzinService.mask(element, format);
+                    }
+
+                    return val;
+                }
+                
+                var _valid = function(val) {
+                    var validade = _attr('validate');
+
+                    if (undefined !== fieldsValidate[validade]) {
+                        var valid = fieldsValidate[validade];
+                        return valid(val);
+                    }
+
+                    return true;
+                }
  
                 // Aplicar a Mascara
-                element.bind("keydown keyup", function () {
-                    if (mask == 'cpf') {
-                        var newVal = FormzinService.mask(element, masksFormat.cpf);
+                element.bind("keyup", function () {
+                    var val = _val();
+                    var valid = _valid(val);
+                    var required = _attr('required');
+
+                    if (required) {
+                        ctrl.$setValidity(_attr('name'), valid);
                     }
-                    
-                    ctrl.$setViewValue(newVal);
+                    ctrl.$setViewValue(val);
                     ctrl.$render();
                 });
 
-                // Fazer a validação
-                element.bind("change", function () {
-                    if (element.attr('required')) {
-                        console.log("sim");
-                    } else {
-                        console.log("não");
-                    }
-                });
-
                 ctrl.$parsers.push(function (value) {
-                    if (mask == 'cpf') {
-                        return FormzinService.parsers(value, mask);
+                    if (_attr('parsers') != "false") {
+                        return FormzinService.parsers(value, _attr('mask'));
                     }
+
+                    return value;
                 });
+                
 
                 ctrl.$formatters.push(function (value) {
-                    if (mask == 'cpf') {
-                        return FormzinService.mask(element, masksFormat.cpf);
-                    }
-                });
+                    return _val();
+                });                                
             }
         };
     }
